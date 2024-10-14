2645a2e9f502dafc1a8e7c38d1a3a7722e49e586
MaxSkydan <skidan.maxim126@gmail.com>
2024-10-15 00:16:18 +0300

    Refactoring

--- services/docker_setup.py
+++ services/docker_setup.py
@@ -1,9 +1,38 @@
 import docker
 import pytest
 import time
+import requests
 from config.settings import *
-from services.rabbit_set import is_rabbitmq_ready_amqp, wait_for_service_ready
+from services.rabbit_set import is_rabbitmq_ready_amqp
+from requests.exceptions import RequestException
 
+
+
+# Проверка готовности сервиса с таймаутом
+def wait_for_service_ready():
+    # Определяем параметры опроса сервиса
+    health_check_url = f"http://{HOST}:8804/actuator/health"  # Эндпоинт для проверки готовности сервиса
+    timeout = 30  # Общее время ожидания в секундах
+    interval = 1  # Интервал между попытками опроса в секундах
+
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        try:
+            response = requests.get(health_check_url)
+            if response.status_code == 200:
+                health_info = response.json()  # Получаем JSON-ответ
+                # Проверяем статус RabbitMQ
+                if health_info.get("components", {}).get("rabbit", {}).get("status") == "UP":
+                    print("Сервис готов к работе!")
+                    return True
+        except RequestException as e:
+            print(f"Ошибка запроса: {e}")
+
+        print(f"Ожидание готовности сервиса... ({time.time() - start_time:.2f}s)")
+        time.sleep(interval)
+
+    print("Время ожидания истекло, сервис не готов.")
+    return False
 
 
 @pytest.fixture(scope="session", autouse=True)
--- services/rabbit_set.py
+++ services/rabbit_set.py
@@ -1,9 +1,7 @@
 import pika, pika.exceptions
 import json
-import requests
 import time
 from config.settings import *
-from requests.exceptions import RequestException
 
 
 
@@ -18,33 +16,6 @@ def is_rabbitmq_ready_amqp():
         return True
     except pika.exceptions.AMQPConnectionError:
         return False
-
-
-# Проверка готовности сервиса с таймаутом
-def wait_for_service_ready():
-    # Определяем параметры опроса сервиса
-    health_check_url = f"http://{HOST}:8804/actuator/health"  # Эндпоинт для проверки готовности сервиса
-    timeout = 30  # Общее время ожидания в секундах
-    interval = 1  # Интервал между попытками опроса в секундах
-
-    start_time = time.time()
-    while time.time() - start_time < timeout:
-        try:
-            response = requests.get(health_check_url)
-            if response.status_code == 200:
-                health_info = response.json()  # Получаем JSON-ответ
-                # Проверяем статус RabbitMQ
-                if health_info.get("components", {}).get("rabbit", {}).get("status") == "UP":
-                    print("Сервис готов к работе!")
-                    return True
-        except RequestException as e:
-            print(f"Ошибка запроса: {e}")
-
-        print(f"Ожидание готовности сервиса... ({time.time() - start_time:.2f}s)")
-        time.sleep(interval)
-
-    print("Время ожидания истекло, сервис не готов.")
-    return False
 
 
 # Подключение к RabbitMQ
