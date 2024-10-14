431c937436996062f15de9f622dc6af27fffeddf
MaxSkydan <skidan.maxim126@gmail.com>
2024-10-15 00:04:51 +0300

    Refactor flask app

--- services/flask_app.py
+++ services/flask_app.py
@@ -7,6 +7,51 @@ from threading import Event, Thread
 # Событие для синхронизации завершения обработки запроса
 flask_event = Event()
 
+SENDER_ID = "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
+RECIPIENT_ID = "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
+
+# Общая функция для формирования ответа и установки события
+def send_response(response_data):
+    flask_event.set()  # Устанавливаем событие после обработки
+    return jsonify(response_data), 200
+
+# Словарь для маппинга сообщений на соответствующие ответы
+response_map = {
+    "#COMMAND:START_CALL": [
+        {"text": "Hello, I speak Ukrainian and English, how can I help you?", "recipient_id": RECIPIENT_ID},
+        {"text": "#UNINTERRUPTIBLE", "recipient_id": RECIPIENT_ID}
+    ],
+    "Де подивитись гарний фільм?": [
+        {"text": "я не можу з цим допомогти , запитайте щось інше", "recipient_id": RECIPIENT_ID},
+        {"text": "#FILE:greeting", "recipient_id": RECIPIENT_ID}
+    ],
+    "Error": [
+        {"text": None, "recipient_id": RECIPIENT_ID}
+    ],
+    "#COMMAND:ERROR": [
+        {"text": "#COMMAND:ERROR", "recipient_id": RECIPIENT_ID}
+    ],
+    "На все добре. До побачення": [
+        {"text": "Приємно було спілкуватись", "recipient_id": RECIPIENT_ID},
+        {"text": "#COMMAND:END_CALL", "recipient_id": RECIPIENT_ID}
+    ],
+    "Щось нове та цікаве": [
+        {"text": "Я вас не розумію. Перевожу на оператора", "recipient_id": RECIPIENT_ID},
+        {"text": "#COMMAND:REDIRECT_CALL:7777", "recipient_id": RECIPIENT_ID}
+    ],
+    "Переведи мене на оператора": [
+        {"text": "Добре. Перевожу на оператора", "recipient_id": RECIPIENT_ID},
+        {"text": "#COMMAND:USER_REDIRECT_CALL", "recipient_id": RECIPIENT_ID}
+    ],
+    "#COMMAND:END_CALL": [
+        {"text": "#COMMAND:END_CALL", "recipient_id": RECIPIENT_ID}
+    ],
+    "Лічильник": [
+        {"text": "Назвіть останні цифри лічильника після звукового сигналу", "recipient_id": RECIPIENT_ID},
+        {"text": "#COMMAND:BEEP", "recipient_id": RECIPIENT_ID}
+    ]
+}
+
 
 @pytest.fixture(scope='session', autouse=True)
 def app() -> Flask:
@@ -18,117 +63,14 @@ def app() -> Flask:
     @app.route('/webhooks/rest/webhook', methods=['POST'])
     def rasa_webhook():
         data = request.json
-        if data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and data["message"] == "#COMMAND:START_CALL":
-            response = [
-                {
-                    "text": "Hello, I speak Ukrainian and English, how can I help you?",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                },
-                {
-                    "text": "#UNINTERRUPTIBLE",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                 }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        elif (data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and
-              data["message"] == "Де подивитись гарний фільм?"):
-            response = [
-                {
-                    "text": "я не можу з цим допомогти , запитайте щось інше",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                },
-                {
-                    "text": "#FILE:greeting",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                 }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        elif data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and data["message"] == "Error":
-            response = [
-                {
-                    "text": None,
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        elif data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and data["message"] == "#COMMAND:ERROR":
-            response = [
-                {
-                    "text": "#COMMAND:ERROR",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        elif (data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and
-              data["message"] == "На все добре. До побачення"):
-            response = [
-                {
-                    "text": "Приємно було спілкуватись",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                },
-                {
-                    "text": "#COMMAND:END_CALL",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        elif (data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and
-              data["message"] == "Щось нове та цікаве"):
-            response = [
-                {
-                    "text": "Я вас не розумію. Перевожу на оператора",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                },
-                {
-                    "text": "#COMMAND:REDIRECT_CALL:7777",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        elif (data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and
-              data["message"] == "Переведи мене на оператора"):
-            response = [
-                {
-                    "text": "Добре. Перевожу на оператора",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                },
-                {
-                    "text": "#COMMAND:USER_REDIRECT_CALL",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        elif data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and data["message"] == "#COMMAND:END_CALL":
-            response = [
-                {
-                    "text": "#COMMAND:END_CALL",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        elif data["sender"] == "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739" and data["message"] == "Лічильник":
-            response = [
-                {
-                    "text": "Назвіть останні цифри лічильника після звукового сигналу",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                },
-                {
-                    "text": "#COMMAND:BEEP",
-                    "recipient_id": "d8bd8e5f-f309-4c13-a569-11c12cfc4b30:7739"
-                }
-            ]
-            flask_event.set()  # Устанавливаем событие после обработки
-            return jsonify(response), 200
-        return jsonify([]), 400
 
+        # Проверяем sender и находим соответствующий ответ по message
+        if data.get("sender") == SENDER_ID:
+            message = data.get("message")
+            if message in response_map:
+                return send_response(response_map[message])
+
+        return jsonify([]), 400
 
     # Start server
     thread = Thread(target=app.run, daemon=True, kwargs=dict(host=host, port=port))
