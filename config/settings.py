f21477b3a4802c6a873816afe33105d8bae906e9
MaxSkydan <skidan.maxim126@gmail.com>
2024-10-20 19:04:07 +0300

    Add env file

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,9 @@
 import socket
+from dotenv import load_dotenv
+import os
+
+# Загружаем переменные из .env
+load_dotenv()
 
 # IP хоста на котором запускаем
 HOST = socket.gethostbyname(socket.gethostname())
@@ -6,8 +11,24 @@ HOST = socket.gethostbyname(socket.gethostname())
 # Настройки RabbitMQ
 RABBITMQ_HOST = HOST
 RABBITMQ_PORT = 5672
-RABBITMQ_USER = 'nlu'
-RABBITMQ_PASSWORD = 'qwerty'
+RABBITMQ_USER = os.getenv('RABBITMQ_USER')
+RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')
+
+#INFLUX DB
+INFLUXDB_ORG = os.getenv('INFLUXDB_ORG')
+INFLUXDB_BUCKET = os.getenv('INFLUXDB_BUCKET')
+INFLUXDB_TOKEN = os.getenv('INFLUXDB_TOKEN')
+INFLUXDB_URL = f"http://{HOST}:8086"
+INFLUXDB_USERNAME = os.getenv('INFLUXDB_USERNAME')
+INFLUXDB_PASSWORD = os.getenv('INFLUXDB_PASSWORD')
+
+#MYSQL
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_DRIVER = os.getenv('DATABASE_DRIVER')
+DATABASE_DIALECT = os.getenv('DATABASE_DIALECT')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_URL = f"jdbc:mysql://{HOST}:3306/{DATABASE_NAME}?useSSL=false&rewriteBatchedStatements=true"
 
 #Настройки NLU_PROXY
 NLU_PROXY_EXCHANGE_INIT = 'svb-nlu-proxy-exchange'
@@ -22,25 +43,8 @@ AUDIT_EXCHANGE = 'svb-audit-exchange'
 AUDIT_ROUTING_KEY = 'svb-audit-key'
 AUDIT_ROUTING_KEY_SEARCH = 'svb-audit-search-key'
 
-#INFLUX DB
-INFLUXDB_ORG = "my_org"
-INFLUXDB_BUCKET = "my_bucket"
-INFLUXDB_TOKEN = "my_token"
-INFLUXDB_URL = f"http://{HOST}:8086"
-INFLUXDB_USERNAME='admin'
-INFLUXDB_PASSWORD='password'
 
-#MYSQL
-DATABASE_URL=f"jdbc:mysql://{HOST}:3306/SMIDDLE?useSSL=false&rewriteBatchedStatements=true"
-DATABASE_USERNAME='root'
-DATABASE_PASSWORD='root_password'
-DATABASE_DRIVER='com.mysql.cj.jdbc.Driver'
-DATABASE_DIALECT='org.hibernate.dialect.MySQLDialect'
-DATABASE_NAME='SMIDDLE'
-
-
-
-# Настройки Docker
+#Настройки Docker
 DOCKER_IMAGE_NLU = 'sm-nexus3.smiddle.lab:5000/smiddlegroup/svb-nlu-proxy:dev'
 CONTAINER_NAME_NLU = 'svb-nlu-proxy'
 DOCKER_IMAGE_AUDIT = 'sm-nexus3.smiddle.lab:5000/smiddlegroup/svb-audit:dev'
